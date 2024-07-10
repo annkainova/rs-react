961654d0bf51a23c6b65d9df94493a15ec7ecc6e
Anna <cainowa@gmail.com>
2024-07-10 18:23:58 +0200

    refactor: converted search screen page to functional component

--- src/pages/mainPage/components/SearchScreen.tsx
+++ src/pages/mainPage/components/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import SearchBar from '../../../components/searchBar/searchBar';
 import getAnime from '../../../api/getAnime';
@@ -7,81 +7,72 @@ import CardSection, { Anime } from './CardSection';
 import Loader from '../../../components/loader/Loader';
 import BuggyButton from '../../../components/buggyButton/buggyButton';
 
-interface SearchScreenState {
-  animeList: Anime[];
-  isLoading: boolean;
-}
+const SearchScreen: React.FC = () => {
+  const [animeList, setAnimeList] = useState<Anime[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-class SearchScreen extends React.Component {
-  state: SearchScreenState = {
-    animeList: [],
-    isLoading: false,
+  const performSearch = async (query: string) => {
+    setIsLoading(true);
+    const animeListResponse = await getAnime(query);
+    setIsLoading(false);
+    setAnimeList(animeListResponse);
   };
 
-  performSearch = async (query: string) => {
-    this.setState({ isLoading: true });
-    const animeList = await getAnime(query);
-    this.setState({ animeList, isLoading: false });
-  };
-
-  async componentDidMount() {
+  useEffect(() => {
     const savedQuery = localStorage.getItem('searchQuery') || '';
-    await this.performSearch(savedQuery);
-  }
+    performSearch(savedQuery);
+  }, []);
 
-  handleClickonLogo = async () => {
+  const handleClickonLogo = async () => {
     localStorage.clear();
-    await this.performSearch('');
+    performSearch('');
   };
 
-  render() {
-    const { animeList, isLoading } = this.state;
-    const defaultImage = 'https://i.imgur.com/i9anCwy.jpg';
-    const coverImage =
-      animeList.length > 0 && animeList[0].attributes.coverImage
-        ? animeList[0].attributes.coverImage.original
-        : defaultImage;
+  const defaultImage = 'https://i.imgur.com/i9anCwy.jpg';
+  const coverImage =
+    animeList.length > 0 && animeList[0].attributes.coverImage
+      ? animeList[0].attributes.coverImage.original
+      : defaultImage;
 
-    return (
-      <main>
-        <section className={cl.searchScreen}>
-          <div className={cl.ButtomError}>
-            <BuggyButton />
-          </div>
-          <h1 onClick={this.handleClickonLogo} className={cl.searchScreen__title}>
-            anime.search
-          </h1>
-          {isLoading && <Loader />}
-
-          <div className="grid">
-            <div className={cl.searchBox}>
-              <SearchBar onSearch={this.performSearch} />
-            </div>
-          </div>
-          <div className="gradient gradient-top"></div>
-          <div className="gradient gradient-left"></div>
-          <div className="gradient gradient-bottom"></div>
-          <img src={coverImage} alt="Anime Poster" />
-        </section>
-        <CardSection animeList={animeList} />
-
-        <nav>
-          <ul>
-            <li>
-              <Link to={`contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div id="detail">
-          <Outlet />
+  return (
+    <main>
+      <section className={cl.searchScreen}>
+        <div className={cl.ButtomError}>
+          <BuggyButton />
         </div>
-      </main>
-    );
-  }
-}
+        <h1 onClick={handleClickonLogo} className={cl.searchScreen__title}>
+          anime.search
+        </h1>
+        {isLoading && <Loader />}
+
+        <div className="grid">
+          <div className={cl.searchBox}>
+            <SearchBar onSearch={performSearch} />
+          </div>
+        </div>
+        <div className="gradient gradient-top"></div>
+        <div className="gradient gradient-left"></div>
+        <div className="gradient gradient-bottom"></div>
+        <img src={coverImage} alt="Anime Poster" />
+      </section>
+      <CardSection animeList={animeList} />
+
+      <nav>
+        <ul>
+          <li>
+            <Link to={`contacts/1`}>Your Name</Link>
+          </li>
+          <li>
+            <Link to={`contacts/2`}>Your Friend</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="detail">
+        <Outlet />
+      </div>
+    </main>
+  );
+};
 
 export default SearchScreen;
