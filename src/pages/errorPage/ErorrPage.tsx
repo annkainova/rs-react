951fd70193f86bc7d7bee04c31d216897fe83f8e
Anna <cainowa@gmail.com>
2024-07-14 20:54:11 +0200

    fix: delete comments

--- src/pages/errorPage/ErorrPage.tsx
+++ src/pages/errorPage/ErorrPage.tsx
@@ -1,13 +1,16 @@
 import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
-  const error = useRouteError();
+  const error = useRouteError() as Error;
   console.error(error);
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.message}</i>
+      </p>
     </div>
   );
 };
