338190ad43f22aa57d7869162e69289c62b01f12
Anna <cainowa@gmail.com>
2024-07-24 16:37:04 +0200

    feat: add error page test

--- src/pages/errorPage/ErorrPage.tsx
+++ src/pages/errorPage/ErorrPage.tsx
@@ -1,16 +1,26 @@
-import { useRouteError } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
+import Button from '../../components/ui/button/Button';
 
 const ErrorPage = () => {
   const error = useRouteError() as Error;
-  console.error(error);
+  console.error('mistake!!:', error);
+
+  const navigate = useNavigate();
+
+  const handleClickReturn = () => {
+    navigate('/');
+  };
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.message}</i>
+        <i>{error && error.message}</i>
       </p>
+      <Button isMain={true} onClick={handleClickReturn}>
+        Return to main
+      </Button>
     </div>
   );
 };
--- src/pages/mainPage/components/__tests__/ErrorPage.test.tsx
+++ src/pages/mainPage/components/__tests__/ErrorPage.test.tsx
@@ -1,36 +1,38 @@
-// import { describe, it, expect, vi } from 'vitest';
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import { MemoryRouter, Route, Routes } from 'react-router-dom';
-// import { QueryClientProvider } from 'react-query';
-// import { Provider } from 'react-redux';
-// import { mockAnimeList, queryClientTest, storeTest } from './mockAnimeList';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import { queryClientTest, storeTest } from './mockAnimeList';
 
-// import FlyoutElement from '../../../../components/FlyoutElement/FlyoutElement';
-// import { setSelectedElements } from '../../../../state/counter/AnimeListSlice';
-// import ErrorPage from '../../../errorPage/ErorrPage';
+import ErrorPage from '../../../errorPage/ErorrPage';
 
-// describe('ErrorPage', () => {
-//   it('Displays an error message when an error occurs', async () => {
-//     render(
-//       <Provider store={storeTest}>
-//         <QueryClientProvider client={queryClientTest}>
-//           <MemoryRouter initialEntries={['/search/1']}>
-//             <Route>
-//               <ErrorPage />
-//             </Route>{' '}
-//             <Routes>
-//               <Route path="/error" element={<ErrorPage />} />
-//             </Routes>
-//           </MemoryRouter>
-//         </QueryClientProvider>
-//       </Provider>
-//     );
+describe('ErrorPage', () => {
+  it('Displays an error message when an error occurs', async () => {
+    const routes = [
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
+    ];
 
-//     expect(screen.queryByText('Oops!')).toBeInTheDocument();
-//     expect(
-//       screen.queryByText('Sorry, an unexpected error has occurred.')
-//     ).toBeInTheDocument();
-//   });
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/error'],
+    });
 
-//   it('', async () => {});
-// });
+    render(
+      <Provider store={storeTest}>
+        <QueryClientProvider client={queryClientTest}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </Provider>
+    );
+
+    expect(screen.getByText('Oops!')).toBeInTheDocument();
+    expect(
+      screen.getByText('Sorry, an unexpected error has occurred.')
+    ).toBeInTheDocument();
+  });
+
+  // it('', async () => {});
+});
